81d6b470700b336cb4ec86b8b8c816e3cacb5f43
illumindesign <info@illumindesign.net>
2017-04-23 14:54:26 -0500

    Create listFiles.sh

---
+++ listFiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+outputFileList () {
+  #LISTFILE=./fileslist.txt << USELESS NOW!!!
+
+  #for f in *
+  #do
+  #  echo "$f" >> $1
+  #done
+  
+  echo "Files list has been saved to: $1";
+}
+
+outputFileList "$1"
